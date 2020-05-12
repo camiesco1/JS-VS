951215bb2fc399208882f13de8c3dfd8d0eece98
Maria Camila Escobar Gonzalez <mescobargonzalez@Marias-iMac.local>
2020-05-12 23:51:09 +0100

    Challenge 6 completed succesfully

--- index.js
+++ index.js
@@ -1,43 +1,49 @@
 /*
 Challenge #5
-Write a function that returns the lowest of two given numbers if both numbers are even, but returns the highest if one or both numbers are odd
+Write a function that returns the lowest of two given numbers if both numbers are even, 
+but returns the highest if one or both numbers are odd
 
 Ie:
 lesser_of_two_givens(2,4) --> 2
 lesser_of_two_givens(2,5) --> 5
-
-If number is even and higher than 6, return the number. If not, return "number's even but lesser than 6"
-If number is odd return 
 */
 
-function lowest (num) {
-    
-    if (num%2==0 > 6)
-        return num;
-    else if(num%2==0 > 6)
-        return "Number is lower than 6";
-    else if (num < 6)
-        return num;
+function even(num) {
+
+    if (num % 2 == 0)
+        return "Even"
     else
-        return "Number is higher than 6"
+        return "Odd"
 }
 
-console.log(lowest(2));
-console.log(lowest(1));
-
-function otherLowest (a, b) {
-    if (a%2==0 && b%2==0)
+function otherLowest(a, b) {
+    if (a % 2 == 0 && b % 2 == 0)
         if (a < b)
-            return a;
+            return a
         else
-            return b;
+            return b
     else
         if (a < b)
-             return b;
+            return b
         else
-            return a;
+            return a
 }
 
-console.log(otherLowest(6, 4));
-console.log(otherLowest(1, 6));
-console.log(otherLowest(1, 5));
+/*
+Challenge #6
+Write a function takes a two-word string and returns True if both words begin with same letter
+
+Ie:
+animal_crackers('Levelheaded Llama') --> True
+animal_crackers('Crazy Kangaroo') --> False
+*/
+
+function twoWords(words) {
+
+    let [a, b] = words.split(" "); // You need to split the words first
+
+    if (a[0] == b[0]) // Almost, this one is missing
+        return true;
+    else
+        return false;
+}
